9dc10fe7c76d4d37e8dffc6c4e9a550db058a79a
Zhbanov Maksim <gbanof96@gmail.com>
2020-01-23 20:11:55 +0300

    feat: Доделал и стилизовал таблицу уровней жреца

--- src/components/UnitProperty.jsx
+++ src/components/UnitProperty.jsx
@@ -11,16 +11,56 @@ export const UnitProperty = props => {
 
   const { value } = currentProperty[0];
 
-  return (
-    <div>
-      {value.map((item, index) => (
-        <div key={index} className="unitProperty">
-          <label>{item.label}</label>
-          {item.image ? <img src={item.image} /> : null}
-          <span className="unitProperty__value">{item.value}</span>
-          <span className="unitProperty__addition">{item.addition}</span>
+  if (value.type === "table") {
+    const { header, body } = value.tableData;
+
+    return (
+      <div>
+        <table className="table">
+          <thead className="table__head">
+            {header.map((row, indexRow) => (
+              <tr key={indexRow}>
+                {row.map((col, indexCol) => {
+                  if (typeof col === "object") {
+                    return (
+                      <th key={indexCol} {...col.attr}>
+                        {col.value}
+                      </th>
+                    );
+                  } else {
+                    return <th key={indexCol}>{col}</th>;
+                  }
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="table__body">
+            {body.map((row, indexRow) => (
+              <tr key={indexRow}>
+                {row.map((col, indexCol) => (
+                  <td key={indexCol}>{col}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="unitProperty">
+          <span className="unitProperty__addition">{value.addition}</span>
         </div>
-      ))}
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {value.data.map((item, index) => (
+          <div key={index} className="unitProperty">
+            <label>{item.label}</label>
+            {item.image ? <img src={item.image} /> : null}
+            <span className="unitProperty__value">{item.value}</span>
+            <span className="unitProperty__addition">{item.addition}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
