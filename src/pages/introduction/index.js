2b066de27df20072baa73ea76b89a8e42fda552d
Zhbanov Maksim <gbanof96@gmail.com>
2020-02-10 16:03:55 +0300

    feat: add parameters data

--- src/pages/introduction/index.js
+++ src/pages/introduction/index.js
@@ -1,4 +1,5 @@
 // @flow
+export type { pageContentType } from './introduction-data'
 export { INTRODUCTION_PAGE_DATA } from './introduction-data'
 export { introductionReducer } from './introduction-reducer'
 export { IntroductionRouter as Inroduction } from './introduction-router'
---
+++ src/pages/parameters/index.js
@@ -0,0 +1,1 @@
+export { PARAMETERS_PAGE } from './parameters-data'
