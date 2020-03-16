4c1e33ce9f48c6e4cf24fc15621f7785b937cc84
Maksim Zhbanov <maksim.zhbanov@gmail.com>
2020-03-17 01:16:03 +0300

    feat: add use skills

--- src/pages/chapter-list/children-page/skills/children-page/index.js
+++ src/pages/chapter-list/children-page/skills/children-page/index.js
@@ -1,4 +1,9 @@
 import { COMMON_OF_SKILLS_PAGE } from './common-of-skills'
 import { GET_SKILLS_PAGE } from './get-skills'
+import { USE_SKILLS_PAGE } from './use-skills'
 
-export const childrenDataList = [COMMON_OF_SKILLS_PAGE, GET_SKILLS_PAGE]
+export const childrenDataList = [
+  COMMON_OF_SKILLS_PAGE,
+  GET_SKILLS_PAGE,
+  USE_SKILLS_PAGE,
+]
