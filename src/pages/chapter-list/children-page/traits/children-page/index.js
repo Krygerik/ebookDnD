d6d5bb23d6acfd20d904eb75626e6edaecccd4f6
Maksim Zhbanov <maksim.zhbanov@gmail.com>
2020-03-31 17:56:13 +0300

    feat: add special traits table

--- src/pages/chapter-list/children-page/traits/children-page/index.js
+++ src/pages/chapter-list/children-page/traits/children-page/index.js
@@ -2,9 +2,11 @@
 import { PURCHARE_TRAITS_PAGE } from './purchare-traits'
 import { NECESSARY_CONDITIONS_PAGE } from './necessary-condition'
 import { TYPES_OF_DISTINCTIVE_PAGE } from './types-of-distinctive'
+import { DESCRIPTION_TRAITS_PAGE } from './description-traits'
 
 export const childrenDataList = [
   PURCHARE_TRAITS_PAGE,
   NECESSARY_CONDITIONS_PAGE,
   TYPES_OF_DISTINCTIVE_PAGE,
+  DESCRIPTION_TRAITS_PAGE,
 ]
