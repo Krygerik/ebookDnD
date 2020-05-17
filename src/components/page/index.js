31c890f3acab41de35d9a56c5309d0c83601788b
Maksim Zhbanov <maksim.zhbanov@gmail.com>
2020-05-17 22:54:11 +0300

    save webpack, flow, eslint, prettier template

--- src/components/page/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-export type {
-  pageContentType,
-  navigationListPageType,
-  tabType,
-} from './page-type'
-export { Page } from './page'
--- src/components/tab-info/index.js
+++
@@ -1,2 +0,0 @@
-// @flow
-export { TabInfo } from './tab-info'
